f2fbe44215f7b39304a260971f7908e321a102c2
Jere <jerespn@gmail.com>
2022-08-22 11:23:09 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/xtended_garden.mk
+   $(LOCAL_DIR)/aicp_garden.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_garden-userdebug \
-    xtended_garden-eng \
-    xtended_garden-user
+    aicp_garden-userdebug \
+    aicp_garden-eng \
+    aicp_garden-user
