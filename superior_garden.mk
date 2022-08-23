3116683ea60e28b8fd30647877d0f18f82a0c39e
Jere <jerespn@gmail.com>
2022-08-23 16:31:04 -0400

    superiorpapa

--- superior_garden.mk
+++ superior_garden.mk
@@ -19,11 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from Nusantara vendor
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Some build flags
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_FACE_UNLOCK := true
 TARGET_USES_BLUR := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
@@ -32,7 +31,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bliss_garden
+PRODUCT_NAME := superior_garden
 PRODUCT_DEVICE := garden
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := xiaomi
