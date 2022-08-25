1f042f7f885e4fde13a186ef1b4b4b3d574b38dc
Jere <jerespn@gmail.com>
2022-08-25 00:57:56 -0400

    aosip

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/nad_garden.mk
+   $(LOCAL_DIR)/aosip_garden.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_garden-userdebug \
-    nad_garden-eng \
-    nad_garden-user
+    aosip_garden-userdebug \
+    aosip_garden-eng \
+    aosip_garden-user
